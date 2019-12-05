a4900e03fa1daad227ae8d688225b136d51dc999
lomasz <lukasz.tomaszewski89@gmail.com>
2019-12-05 14:00:57 +0100

    Remove serialization in SearchResult

--- src/main/java/com/lomasz/spring/boot/template/model/dto/SearchResult.java
+++ src/main/java/com/lomasz/spring/boot/template/model/dto/SearchResult.java
@@ -3,19 +3,20 @@ package com.lomasz.spring.boot.template.model.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.io.Serializable;
 import java.util.List;
 
 @Data
 @Builder
-public class SearchResult<T> implements Serializable {
-
-    private static final long serialVersionUID = 8638435681375964215L;
+public class SearchResult<T> {
 
     private List<T> items;
+
     private Long totalCount;
+
     private Integer page;
+
     private Integer limit;
+
     private Integer pages;
 
 }
