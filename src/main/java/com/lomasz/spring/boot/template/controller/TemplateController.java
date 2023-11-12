1ba46c2505d54d78d801105d48944a45727b9f92
lomasz <lukasz.tomaszewski89@gmail.com>
2023-11-12 23:15:00 +0100

    add OpenAPI description for endpoints

--- src/main/java/com/lomasz/spring/boot/template/controller/TemplateController.java
+++ src/main/java/com/lomasz/spring/boot/template/controller/TemplateController.java
@@ -1,5 +1,6 @@
 package com.lomasz.spring.boot.template.controller;
 
+import com.lomasz.spring.boot.template.model.dto.ErrorDto;
 import com.lomasz.spring.boot.template.model.dto.NewTemplateDto;
 import com.lomasz.spring.boot.template.model.dto.SearchResult;
 import com.lomasz.spring.boot.template.model.dto.TemplateDto;
@@ -31,6 +32,12 @@ public class TemplateController {
     private final TemplateService templateService;
 
     @GetMapping
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200"),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+            })
     public ResponseEntity<SearchResult<TemplateDto>> search(
             @RequestParam(name = "page", required = false, defaultValue = "0") int page,
             @RequestParam(name = "size", required = false, defaultValue = "20") int size,
@@ -40,6 +47,12 @@ public class TemplateController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Created. 'Location' header contains URL of the new resource."),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+            })
     public ResponseEntity<Void> add(@RequestBody @Valid NewTemplateDto newDto) {
         Long id = templateService.create(newDto);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -53,8 +66,10 @@ public class TemplateController {
     @GetMapping("/{id}")
     @ApiResponses(
             value = {
-                @ApiResponse(responseCode = "200"),
-                @ApiResponse(responseCode = "404", content = @Content(schema = @Schema))
+                    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = TemplateDto.class))),
+                    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+                    @ApiResponse(responseCode = "404"),
+                    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorDto.class))),
             })
     public ResponseEntity<TemplateDto> getById(@PathVariable("id") Long id) {
         return templateService.findById(id)
